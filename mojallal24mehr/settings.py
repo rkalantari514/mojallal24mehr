9256e487bc3de5993179f704e908717c7a7188f0
rkalantari <rkalantari514@gmail.com>
2024-11-16 12:23:09 +0330

    collectstatic

--- mojallal24mehr/settings.py
+++ mojallal24mehr/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--o*rtwaxd%_%yz7t2505stw(r_l$ftjl^7l$c_nr5k4#hcjgj*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','172.16.1.14']
 
