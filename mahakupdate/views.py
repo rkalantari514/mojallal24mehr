b00866361d7accf8a56a2aa9efef77cf4223a010
rkalantari <rkalantari514@gmail.com>
2024-11-26 15:27:43 +0330

    mojodi 0906 2

--- mahakupdate/views.py
+++ mahakupdate/views.py
@@ -1259,7 +1259,7 @@ from django.db.models import Q
 def UpdateMojodi(request):
     # بارگذاری داده‌ها از مدل Kardex
     # kardex_entries = Kardex.objects.all().select_related('storage', 'kala')
-    kardex_entries = Kardex.objects.filter(code_kala=67143).select_related('storage', 'kala')
+    kardex_entries = Kardex.objects.order_by('date','radif').select_related('storage', 'kala')
     mm=0
     for k in kardex_entries:
         print(k.pdate,k.warehousecode,k.radif,k.count)
