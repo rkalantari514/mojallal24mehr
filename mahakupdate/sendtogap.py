093ec71d5480a33e0780a59dc3c4b09537c6296c
rkalantari-hp <rkalantari514@gmail.com>
2024-12-28 21:07:43 +0330

    send_to_managers 1

--- mahakupdate/sendtogap.py
+++ mahakupdate/sendtogap.py
@@ -31,7 +31,7 @@ def send_to_managers(data):
             'form': None,
         }
         myheader = {
-            'token': 'e55bcc5a1e961e9cba1d131de91d2b4aa2a7fa7ea68ad8f44dfe687986dba690'
+            'token': 'a9827aaae459d060d8ca68f0be07f58f1b39eebe29f457da25f2ceed731ddbf1'
         }
         response = requests.post('https://api.gap.im/sendMessage/', data=myparams, headers=myheader)
 
