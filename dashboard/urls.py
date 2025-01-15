54c46dd92edb8e2bac18d282e55a3aee0045fdd3
rkalantari-hp <rkalantari514@gmail.com>
2025-01-15 22:09:56 +0330

    سود ناویژه

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 
 from dashkala.views import DsshKala
-from .views import Home1, CreateReport
+from .views import CreateReport, Home1
 from mahaktables.views import MTables
 
 
 
 
 urlpatterns = [
-    # path('', Home1,name="home1"),
+    path('', Home1,name="home1"),
+
+
+
+    # path('', Home5,name="home1"),
     # path('', DsshKala,name="home1"),
 
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,28 +1,113 @@
-from django.shortcuts import render
-
-from mahakupdate.models import Factor, FactorDetaile, SanadDetail
-from django.db.models import Sum
-from django.contrib.auth.decorators import login_required
-
-from django.shortcuts import render, redirect
-from django.utils import timezone
-from .models import MasterInfo, MasterReport
-import jdatetime  # برای کار با تاریخ جلالی
-from datetime import datetime, timedelta
-import time
-from django.shortcuts import render
-from django.utils import timezone
-from .models import MasterInfo, MasterReport
-import jdatetime  # برای کار با تاریخ جلالی
-from datetime import datetime, timedelta
-import time
 import logging
-# Create your views here.
+logger = logging.getLogger(__name__)
+from custom_login.models import UserLog
+from mahakupdate.models import Factor, FactorDetaile, SanadDetail, Mtables
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import MasterInfo, MasterReport
+from django.utils import timezone
+import jdatetime
+import time
+from django.db.models import Sum
+from datetime import date, timedelta
+
+from django.db.models import Sum, Q
+
+from django.db.models import Q
+
+
+from collections import defaultdict
+
+from collections import defaultdict
+from django.db.models import Sum, Q
+from datetime import timedelta
+
+def TarazCal(fday, lday):
+    # ایجاد لیستی از تمام روزهای بین fday و lday
+    day_range = [fday + timedelta(days=x) for x in range((lday - fday).days + 1)]
+
+    # فیلتر کردن داده‌ها برای تمام روزها در یک بار
+    data = SanadDetail.objects.filter(
+        date__range=(fday, lday)
+    ).filter(
+        Q(kol=500) | Q(kol=400)
+    ).values('date', 'kol').annotate(total_amount=Sum('curramount'))
+
+    sood_navizhe = 0
+    active_day = 0
+    daily_sood_navizhe = []  # لیست برای ذخیره مقادیر روزانه
+
+    # استفاده از defaultdict برای ذخیره‌سازی داده‌ها
+    current_data = defaultdict(int)
+    for item in data:
+        current_data[(item['date'], item['kol'])] = item['total_amount']
+
+    for current_date in day_range:
+        baha_tamam_forosh = current_data.get((current_date, 500), 0)
+        daramad_forosh = current_data.get((current_date, 400), 0)
+
+        # محاسبه مجموع روزانه
+        daily_total = daramad_forosh + baha_tamam_forosh
+        daily_sood_navizhe.append(daily_total)  # ذخیره مقدار روزانه
+
+        if daramad_forosh != 0 or baha_tamam_forosh != 0:
+            active_day += 1
+
+        sood_navizhe += daily_total
+
+    # محاسبه حداقل و حداکثر
+    min_sood_navizhe = min(daily_sood_navizhe) / 10000000 if daily_sood_navizhe else 0
+    max_sood_navizhe = max(daily_sood_navizhe) / 10000000 if daily_sood_navizhe else 0
+
+    to_return = {
+        'sood_navizhe': sood_navizhe / 10000000,
+        'active_day': active_day,
+        'ave_sood_navizhe': sood_navizhe / active_day / 10000000 if active_day > 0 else 0,
+        'min_sood_navizhe': min_sood_navizhe,
+        'max_sood_navizhe': max_sood_navizhe,
+    }
+
+    return to_return
+@login_required(login_url='/login')
+def Home1(request, *args, **kwargs):
+    user=request.user
+    if user.mobile_number != '09151006447':
+        UserLog.objects.create(
+            user=user,
+            page='داشبورد 1',
+        )
+    start_time = time.time()  # زمان شروع تابع
+
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    acc_year = MasterInfo.objects.filter(is_active=True).last().acc_year
+    start_date_jalali = jdatetime.date(acc_year, 1, 1)  # ۱ فروردین سال مالی
+    start_date_gregorian = start_date_jalali.togregorian()  # تبدیل به میلادی
+    last_update_time=Mtables.objects.filter(name='Sanad_detail').last().last_update_time
+    today_data=TarazCal(today,today)
+    yesterday_data=TarazCal(yesterday,yesterday)
+    allday_data=TarazCal(start_date_gregorian,today)
+    print(today_data)
+    print(yesterday_data)
+    context = {
+        'title': 'داشبورد مدیرریتی',
+        'user': user,
+        'last_update_time': last_update_time,
+        'today_data': today_data,
+        'yesterday_data': yesterday_data,
+        'allday_data': allday_data,
+
+    }
+
+    total_time = time.time() - start_time  # محاسبه زمان اجرا
+    print(f"زمان کل اجرای تابع: {total_time:.2f} ثانیه")
+    return render(request, 'home1.html', context)
+
 
 
 
 @login_required(login_url='/login')
-def Home1(request):
+def Home5(request):
     user=request.user
 
     factor=Factor.objects.all()
@@ -94,34 +179,8 @@ def Home1(request):
 
 
 
-# تنظیمات لاگ‌گیری
-
-from django.shortcuts import render
-from django.utils import timezone
-from .models import MasterInfo, MasterReport
-import jdatetime  # برای کار با تاریخ جلالی
-from datetime import datetime, timedelta
-import time
-import logging
-
-# تنظیمات لاگ‌گیری
-
-from django.utils import timezone
-from django.db.models import Sum, Q
-from datetime import timedelta
-import jdatetime
-import time
-import logging
 
 
-from django.utils import timezone
-from django.db.models import Sum
-from datetime import timedelta
-import jdatetime
-import time
-import logging
-
-logger = logging.getLogger(__name__)
 
 def CreateReport(request):
     start_time = time.time()  # زمان شروع ویو
