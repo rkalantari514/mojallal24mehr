d5069297dbde2fccf4c9144aae2022903272c5d6
rkalantari-hp <rkalantari514@gmail.com>
2025-01-27 22:16:56 +0330

    all mahak table 3

--- mojallal24mehr/settings.py
+++ mojallal24mehr/settings.py
@@ -78,7 +78,8 @@ ROOT_URLCONF = 'mojallal24mehr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        # 'DIRS': [BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
