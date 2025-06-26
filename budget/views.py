7fbab69ad0368e507d2fa2829d2639b07da99322
rkalantari-hp <rkalantari514@gmail.com>
2025-06-26 22:45:05 +0330

    multilevel-tree-map 1

--- budget/views.py
+++ budget/views.py
@@ -1166,15 +1166,11 @@ def BudgetSaleTotal(request, *args, **kwargs):
         'amalkard2': amalkard2,
     })
 
-
-
-
-
-
-
-
-
-
+    # فرض بر این است که این کد در ویو Django یا فریمورک مشابه است
+    table10 = [{'l1': item['l1'], 'cy_factor': item['cy_factor']} for item in table1 if item.get('cy_factor', 0) > 0]
+    table20 = [{'l1': item['l1'], 'l2': item['l2'], 'cy_factor': item['cy_factor']} for item in table2 if item.get('cy_factor', 0) > 0]
+    table30 = [{'l1': item['l1'], 'l2': item['l2'], 'l3': item['l3'], 'cy_factor': item['cy_factor']} for item
+                       in table3 if item.get('cy_factor', 0) > 0]
 
     context = {
         'acc_year': acc_year,
@@ -1184,6 +1180,9 @@ def BudgetSaleTotal(request, *args, **kwargs):
         'table2': table2,
         'table1': table1,
         'table0': table0,
+        'table10': table10,
+        'table20': table20,
+        'table30': table30,
 
     }
 
