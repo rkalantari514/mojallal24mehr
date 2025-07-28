de437283fbe4b4ef3bec1d1a0bf0b435e5c7fcee
rkalantari-hp <rkalantari514@gmail.com>
2025-07-28 21:53:50 +0330

    acc_days 3

--- accounting/views.py
+++ accounting/views.py
@@ -1537,7 +1537,7 @@ def HesabMoshtariDetail(request, tafsili):
     khab=True
     if hesabmoshtari.sleep_investment and hesabmoshtari.sleep_investment >= 0:
         khab=False
-    bar_mali=hesabmoshtari.sleep_investment/30* 0.05
+    bar_mali=hesabmoshtari.sleep_investment/30* decimal(0.05)
 
     context = {
         'title': 'حساب مشتری',
